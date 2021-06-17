8735dabb34af87c53945ea2a2997bcd8f60b8c03
Samuel Delepoulle <samuel.delepoulle@univ-littoral.fr>
2021-06-17 15:53:49 +0200

    fix link

--- src/index.php
+++ src/index.php
@@ -1,6 +1,5 @@
 <?php
-echo "Hello from JrCan.dev <br>";
+echo "Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 ?>
 
-<a href="https://jrcan.dev.netlib.re/">JrCanDev</a>
