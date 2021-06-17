ee068efd1e366ad04f3592dc8d1015d719ef6d2c
Samuel Delepoulle <samuel.delepoulle@univ-littoral.fr>
2021-06-17 15:41:12 +0200

    add redeploy.sh

---
+++ redeploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git pull origin main
+
+docker-compose up -d
+
+#docker pull example/repository:latest
+#docker stop SAMPLE_APP
+#docker system prune -f
+#docker run -d --name=SAMPLE_APP example/repository:latest
+
+
