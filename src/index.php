cbea5b5bcab84818c2e4e0c79a4c4e5716a10911
Samuel Delepoulle <samuel.delepoulle@univ-littoral.fr>
2021-10-05 18:58:10 +0200

    suppress phpinfo

--- src/index.php
+++ src/index.php
@@ -1,7 +1,6 @@
 <?php
 echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
-echo '<p>update : 04 oct. 2021 </p>';
-echo phpinfo() ;
+echo '<p>update : 05 oct. 2021 </p>';
 ?>
 
