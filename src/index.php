c08f452e37177dda3a0aa4201f088a2ba65abef8
Samuel Delepoulle <samuel.delepoulle@univ-littoral.fr>
2021-10-05 18:18:01 +0200

    add php version

--- src/index.php
+++ src/index.php
@@ -2,5 +2,6 @@
 echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 echo '<p>update : 04 oct. 2021 </p>';
+echo phpinfo() ;
 ?>
 
