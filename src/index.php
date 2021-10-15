4b00813f45f00efe9c9e5967b326073a9845b71a
Vandewiele Franck <franck.vandewiele@univ-littoral.fr>
2021-10-15 14:59:39 +0200

    mysql + script php de base

--- src/index.php
+++ src/index.php
@@ -7,5 +7,9 @@ echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 echo '<p>update : 05 oct. 2021 </p>';
 
 
+$db = mysqli_connect($_ENV['DB_HOST'], 'test', '1234567abc', 'testdb');
+
+var_dump($db);
+
 ?>
 
