3421b690ac3a8f37f925ab7f019b2c6645df4681
Vandewiele Franck <franck.vandewiele@univ-littoral.fr>
2021-10-15 15:57:03 +0200

    gagné

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@
 echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 echo '<p>update : 05 oct. 2021 </p>';
-echo '<a href="http://phddb.jrcandev.netlib.re:9123/hooks/b3b981be">refresh</a>';
+
 
 echo '<p>$_ENV</p>';
 print_r($_ENV);
