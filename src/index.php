a1d47662120cfe2a37793af94c3fc55def6e2812
Samuel Delepoulle <samuel.delepoulle@univ-littoral.fr>
2021-11-23 18:47:40 +0100

    add phpinfo

--- src/index.php
+++ src/index.php
@@ -2,5 +2,6 @@
 echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 echo '<p>update : 23 nov. 2021 </p>';
+phpinfo();
 ?>
 
