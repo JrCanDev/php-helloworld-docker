ccf741c0b052c9067f1c849e6cf6e3c66b0e1a5f
Samuel Delepoulle <samuel.delepoulle@univ-littoral.fr>
2021-11-23 18:20:09 +0100

    change date

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
-echo '<p>update : 05 oct. 2021 </p>';
+echo '<p>update : 23 nov. 2021 </p>';
 ?>
 
