88d4ae039d67c7d0395282bd09d795f1c1f68590
Samuel Delepoulle <samuel.delepoulle@univ-littoral.fr>
2021-12-02 16:24:40 +0100

    date au 2 déc

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
-echo '<p>update : 23 nov. 2021 </p>';
+echo '<p>update : 2 déc. 2021 </p>';
 phpinfo();
 ?>
 
