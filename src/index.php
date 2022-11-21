faddc3814eb5f62bc0d850eb3e57da75b8721d46
Samuel Delepoulle <samuel.delepoulle@univ-littoral.fr>
2022-11-21 20:44:06 +0100

    correct code

--- src/index.php
+++ src/index.php
@@ -3,7 +3,9 @@
 $username = $_ENV['database__connection__user'];
 $password = $_ENV['database__connection__password'];
 $database = $_ENV['database__connection__database'];
-$pdo = new PDO('mysql:host=www_db;port=3306;dbname=testdb', $username, $password) or die('cannot instantiate PDO instance');
+$name_db = $_ENV['container__name']."_db";
+$connect = "mysql:host=$name_db;port=3306;dbname=$database";
+$pdo = new PDO($connect, $username, $password) or die('cannot instantiate PDO instance');
 
 $sql = "CREATE TABLE IF NOT EXISTS Data (
     id INT AUTO_INCREMENT PRIMARY KEY,
